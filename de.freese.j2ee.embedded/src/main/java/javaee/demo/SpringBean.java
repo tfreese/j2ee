dba8a93ad703b22d9c2d90373374a5e38a4a11ec
tfreese <commercial@freese-home.de>
2014-01-10 18:15:11 +0100

    .classpath und j2ee.embedded aktualisiert

---
+++ de.freese.j2ee.embedded/src/main/java/javaee/demo/SpringBean.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package javaee.demo;
+
+import java.io.Serializable;
+
+/**
+ * @author Thomas Freese
+ */
+public class SpringBean implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4200733042440646149L;
+
+	/**
+	 * 
+	 */
+	private String value = null;
+
+	/**
+	 * 
+	 */
+	public SpringBean()
+	{
+		super();
+	}
+
+	/**
+	 * @return the value
+	 */
+	public String getValue()
+	{
+		if (this.value == null)
+		{
+			this.value = getClass().getName();
+
+		}
+
+		return this.value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(final String value)
+	{
+		this.value = value;
+	}
+}
