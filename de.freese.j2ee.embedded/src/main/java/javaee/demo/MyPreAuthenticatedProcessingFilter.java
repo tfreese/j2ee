fe998f4de4e40c3de7e3f121ad4f13c663b8f932
tfreese <commercial@freese-home.de>
2014-01-20 17:14:45 +0100

    spring security für j2ee.embedded

---
+++ de.freese.j2ee.embedded/src/main/java/javaee/demo/MyPreAuthenticatedProcessingFilter.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package javaee.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+/**
+ * Es wird der gesamte Request als Principal genommen.
+ * 
+ * @author Thomas Freese
+ */
+public class MyPreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedProcessingFilter
+{
+	/**
+	 * 
+	 */
+	public MyPreAuthenticatedProcessingFilter()
+	{
+		super();
+	}
+
+	/**
+	 * @see org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter#getPreAuthenticatedCredentials(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	protected Object getPreAuthenticatedCredentials(final HttpServletRequest request)
+	{
+		return request;
+	}
+
+	/**
+	 * @see org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter#getPreAuthenticatedPrincipal(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	protected Object getPreAuthenticatedPrincipal(final HttpServletRequest request)
+	{
+		return "";
+	}
+}
