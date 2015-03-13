6eb68d83af949215e7bbb137e3ed194129a95e3a
tfreese <commercial@freese-home.de>
2015-03-13 11:24:13 +0100

    - aufgeräumt Signed-off-by:tfreese <commercial@freese-home.de>

--- de.freese.j2ee.embedded/src/main/java/javaee/demo/MyAuthenticationUserDetailsService.java
+++ de.freese.j2ee.embedded/src/main/java/javaee/demo/MyAuthenticationUserDetailsService.java
@@ -1,7 +1,6 @@
 /**
  * Created: 21.01.2014
  */
-
 package javaee.demo;
 
 import java.util.Arrays;
@@ -22,47 +21,48 @@ import org.springframework.security.web.authentication.preauth.PreAuthenticatedA
  */
 public class MyAuthenticationUserDetailsService implements UserDetailsService, AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken>
 {
-	/**
-	 * Erstellt ein neues {@link MyAuthenticationUserDetailsService} Object.
-	 */
-	public MyAuthenticationUserDetailsService()
-	{
-		super();
-	}
+    /**
+     * Erstellt ein neues {@link MyAuthenticationUserDetailsService} Object.
+     */
+    public MyAuthenticationUserDetailsService()
+    {
+        super();
+    }
 
-	/**
-	 * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
-	 */
-	@Override
-	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException
-	{
-		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+    /**
+     * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
+     */
+    @Override
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException
+    {
+        Set<GrantedAuthority> authorities = new HashSet<>();
 
-		for (String role : Arrays.asList("ROLE_A", "ROLE_B"))
-		{
-			authorities.add(new SimpleGrantedAuthority(role));
-		}
+        for (String role : Arrays.asList("ROLE_A", "ROLE_B"))
+        {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
 
-		UserDetails userDetails = new User(username, "7c4a8d09ca3762af61e59520943dc26494f8941b", authorities);
+        // Password = 123456 (echo -n 123456 | sha256sum)
+        UserDetails userDetails = new User(username, "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", authorities);
 
-		return userDetails;
-	}
+        return userDetails;
+    }
 
-	/**
-	 * @see org.springframework.security.core.userdetails.AuthenticationUserDetailsService#loadUserDetails(org.springframework.security.core.Authentication)
-	 */
-	@Override
-	public UserDetails loadUserDetails(final PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException
-	{
-		if (!(token.getPrincipal() instanceof HttpServletRequest))
-		{
-			throw new IllegalArgumentException("Principal ist kein HttpServletRequest");
-		}
+    /**
+     * @see org.springframework.security.core.userdetails.AuthenticationUserDetailsService#loadUserDetails(org.springframework.security.core.Authentication)
+     */
+    @Override
+    public UserDetails loadUserDetails(final PreAuthenticatedAuthenticationToken token) throws UsernameNotFoundException
+    {
+        if (!(token.getPrincipal() instanceof HttpServletRequest))
+        {
+            throw new IllegalArgumentException("Principal ist kein HttpServletRequest");
+        }
 
-		// HttpServletRequest request = (HttpServletRequest) token.getPrincipal();
-		// String userID = request.getHeader("My-UserID");
-		String userID = "täschtd";
+        // HttpServletRequest request = (HttpServletRequest) token.getPrincipal();
+        // String userID = request.getHeader("My-UserID");
+        String userID = "täschtd";
 
-		return loadUserByUsername(userID);
-	}
+        return loadUserByUsername(userID);
+    }
 }
