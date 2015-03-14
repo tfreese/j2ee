d5f9bfd454af71351105fbad582f6f293f088ef4
tfreese <commercial@freese-home.de>
2015-03-14 12:27:28 +0100

    ejb-embedded projekt

---
+++ de.freese.ejb.embedded/src/test/java/de/freese/ejb/embedded/bean/HelloWorldTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.freese.ejb.embedded.bean;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Thomas Freese
+ */
+public class HelloWorldTest
+{
+    private static EJBContainer ejbContainer = null;
+
+    private static Context ctx = null;
+
+    public HelloWorldTest()
+    {
+        super();
+    }
+
+    @BeforeClass
+    public static void setUpClass()
+    {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(EJBContainer.MODULES, new File("target/classes"));
+        ejbContainer = EJBContainer.createEJBContainer(properties);
+//        ejbContainer = EJBContainer.createEJBContainer();
+        ctx = ejbContainer.getContext();
+    }
+
+    @AfterClass
+    public static void tearDownClass()
+    {
+        ejbContainer.close();
+    }
+
+    @Before
+    public void setUp()
+    {
+    }
+
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testHelloWorld()
+    {
+        try
+        {
+            IHelloWorldLocal helloWorld = (IHelloWorldLocal) ctx.lookup("java:global/classes/HelloWorldEJB!de.freese.ejb.embedded.bean.IHelloWorldLocal");
+            assertNotNull(helloWorld);
+
+            String value = helloWorld.getHelloWorld();
+            assertNotNull(value);
+            assertEquals("Hello World", value);
+        }
+        catch (NamingException ex)
+        {
+            throw new AssertionError(ex);
+        }
+    }
+}
