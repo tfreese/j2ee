3a343e6d28841ae0b7659a36fbb2e0e39d29e0f2
tfreese <commercial@freese-home.de>
2015-03-23 22:17:18 +0100

    ejbspring aktualisiert

--- de.freese.ejbspring/ejbspring-jms/src/main/java/de/freese/ejbspring/jms/TransferListener.java
+++ de.freese.ejbspring/ejbspring-jms/src/main/java/de/freese/ejbspring/jms/TransferListener.java
@@ -1,8 +1,5 @@
 package de.freese.ejbspring.jms;
 
-import de.freese.ejbspring.facade.IMoneyTransferFacade;
-import de.freese.ejbspring.facade.IMoneyTransferResponse;
-import de.freese.ejbspring.facade.impl.MoneyTransferRequestImpl;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
@@ -11,115 +8,109 @@ import javax.jms.MessageListener;
 import javax.jms.Session;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
+import de.freese.ejbspring.facade.IMoneyTransferFacade;
+import de.freese.ejbspring.facade.IMoneyTransferResponse;
+import de.freese.ejbspring.facade.impl.MoneyTransferRequestImpl;
 
 /**
- *
  * JMS-listener zum Geldtransfer.
- *
+ * 
  * @author Thomas Freese
- *
  */
 public class TransferListener implements MessageListener
 {
-    /**
+	/**
      *
      */
-    private IMoneyTransferFacade transferFacade = null;
+	private JmsTemplate jmsTemplate = null;
 
-    /**
+	/**
      *
      */
-    private JmsTemplate jmsTemplate = null;
+	private IMoneyTransferFacade transferFacade = null;
 
-    /**
+	/**
      *
      */
-    public TransferListener()
-    {
-        super();
-    }
+	public TransferListener()
+	{
+		super();
+	}
 
-    /**
-     *
-     * @param transferService IMoneyTransferFacade
-     */
-    public void setTransferFacade(IMoneyTransferFacade transferFacade)
-    {
-        this.transferFacade = transferFacade;
-    }
+	/**
+	 * @param message Message
+	 */
+	@Override
+	public synchronized void onMessage(final Message message)
+	{
+		if (message instanceof MapMessage)
+		{
+			MapMessage mapMessage = (MapMessage) message;
 
-    /**
-     *
-     * @param jmsTemplate JmsTemplate
-     */
-    public void setJmsTemplate(JmsTemplate jmsTemplate)
-    {
-        this.jmsTemplate = jmsTemplate;
-    }
+			try
+			{
+				String konto = mapMessage.getString("konto");
+				Double betrag = mapMessage.getDouble("betrag");
 
-    /**
-     *
-     * @param message Message
-     */
-    @Override
-    public synchronized void onMessage(Message message)
-    {
-        if (message instanceof MapMessage)
-        {
-            MapMessage mapMessage = (MapMessage) message;
+				IMoneyTransferResponse response = this.transferFacade.transfer(new MoneyTransferRequestImpl(konto, betrag));
 
-            try
-            {
-                String konto = mapMessage.getString("konto");
-                Double betrag = mapMessage.getDouble("betrag");
+				replyTo(message.getJMSReplyTo(), response.getKontostand().intValue(), "OK");
+			}
+			catch (Throwable th)
+			{
+				th.printStackTrace();
 
-                IMoneyTransferResponse response = this.transferFacade.transfer(new MoneyTransferRequestImpl(konto, betrag));
+				try
+				{
+					replyTo(message.getJMSReplyTo(), -1, th.getMessage());
+				}
+				catch (JMSException ex)
+				{
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
 
-                replyTo(message.getJMSReplyTo(), response.getKontostand().intValue(), "OK");
-            }
-            catch (Throwable th)
-            {
-                th.printStackTrace();
+	/**
+	 * Reply to a destination with a certain status code and status message
+	 * 
+	 * @param destination - where to reply to
+	 * @param code - 0 for success, non-zero otherwise
+	 * @param message - message regarding operation status
+	 */
+	private void replyTo(final Destination destination, final int code, final String message)
+	{
+		if (destination != null)
+		{
+			this.jmsTemplate.send(destination, new MessageCreator()
+			{
+				@Override
+				public Message createMessage(final Session session) throws JMSException
+				{
+					MapMessage mapMessage = session.createMapMessage();
+					mapMessage.setIntProperty("code", code);
+					mapMessage.setStringProperty("msg", message);
 
-                try
-                {
-                    replyTo(message.getJMSReplyTo(), -1, th.getMessage());
-                }
-                catch (JMSException ex)
-                {
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }
+					return mapMessage;
+				}
+			});
+		}
+	}
 
-    /**
-     * Reply to a destination with a certain status code and status message
-     *
-     * @param destination
-     *                    - where to reply to
-     * @param code
-     *                    - 0 for success, non-zero otherwise
-     * @param message
-     *                    - message regarding operation status
-     */
-    private void replyTo(Destination destination, final int code,
-                         final String message)
-    {
-        if (destination != null)
-        {
-            this.jmsTemplate.send(destination, new MessageCreator()
-            {
-                public Message createMessage(Session session)
-                        throws JMSException
-                {
-                    MapMessage mapMessage = session.createMapMessage();
-                    mapMessage.setIntProperty("code", code);
-                    mapMessage.setStringProperty("msg", message);
+	/**
+	 * @param jmsTemplate JmsTemplate
+	 */
+	public void setJmsTemplate(final JmsTemplate jmsTemplate)
+	{
+		this.jmsTemplate = jmsTemplate;
+	}
 
-                    return mapMessage;
-                }
-            });
-        }
-    }
+	/**
+	 * @param transferFacade {@link IMoneyTransferFacade}
+	 */
+	public void setTransferFacade(final IMoneyTransferFacade transferFacade)
+	{
+		this.transferFacade = transferFacade;
+	}
 }
