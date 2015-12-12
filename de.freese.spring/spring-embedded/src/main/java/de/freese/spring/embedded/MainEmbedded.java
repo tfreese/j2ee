593f5cce721c4777c4c8056179d608dd6ee4ee96
tfreese <commercial@freese-home.de>
2015-12-12 13:13:18 +0100

    Neue Version von QueryDSL

--- de.freese.spring/spring-embedded/src/main/java/de/freese/spring/embedded/MainEmbedded.java
+++ de.freese.spring/spring-embedded/src/main/java/de/freese/spring/embedded/MainEmbedded.java
@@ -1,8 +1,8 @@
 package de.freese.spring.embedded;
 
-import com.mysema.query.codegen.JavaTypeMappings;
-import com.mysema.query.sql.codegen.ExtendedBeanSerializer;
-import com.mysema.query.sql.codegen.MetaDataExporter;
+import com.querydsl.codegen.JavaTypeMappings;
+import com.querydsl.sql.codegen.ExtendedBeanSerializer;
+import com.querydsl.sql.codegen.MetaDataExporter;
 import java.io.File;
 import java.sql.Connection;
 import java.util.List;
@@ -16,50 +16,51 @@ import org.springframework.jdbc.datasource.DataSourceUtils;
  */
 public class MainEmbedded
 {
-	/**
-	 * @param args String[]
-	 * @throws Exception Falls was schief geht.
-	 */
-	public static void main(final String[] args) throws Exception
-	{
-		// System.setProperty("org.slf4j.simpleLogger.log.org.springframework.jdbc.core.JdbcTemplate", "debug");
+    /**
+     * @param args String[]
+     *
+     * @throws Exception Falls was schief geht.
+     */
+    public static void main(final String[] args) throws Exception
+    {
+        // System.setProperty("org.slf4j.simpleLogger.log.org.springframework.jdbc.core.JdbcTemplate", "debug");
 
-		try (AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml"))
-		{
-			applicationContext.registerShutdownHook();
+        try (AbstractApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml"))
+        {
+            applicationContext.registerShutdownHook();
 
-			// DataSource dataSource = applicationContext.getBean("dataSource",DataSource.class);
-			MyTableDAO dao = applicationContext.getBean("daoMyTable", MyTableDAO.class);
-			System.out.println(dao.nextSequence());
+            // DataSource dataSource = applicationContext.getBean("dataSource",DataSource.class);
+            MyTableDAO dao = applicationContext.getBean("daoMyTable", MyTableDAO.class);
+            System.out.println(dao.nextSequence());
 
-			dao.insert();
+            dao.insert();
 
-			List<Map<String, Object>> result = dao.select();
+            List<Map<String, Object>> result = dao.select();
 
-			for (Map<String, Object> row : result)
-			{
-				System.out.println(row);
-			}
+            for (Map<String, Object> row : result)
+            {
+                System.out.println(row);
+            }
 
-			dao.backup();
+            dao.backup();
 
-			// QueryDSL
-			MetaDataExporter exporter = new MetaDataExporter();
-			// exporter.setConfiguration(configuration);
-			exporter.setNamePrefix("Q");
-			// exporter.setNamingStrategy(namingStrategy);
-			exporter.setTypeMappings(new JavaTypeMappings());
-			exporter.setBeanSerializer(new ExtendedBeanSerializer());
-			exporter.setPackageName("de.freese.spring.embedded.domain");
-			exporter.setTargetFolder(new File("src/main/generated"));
-			exporter.setColumnAnnotations(true);
-			exporter.setValidationAnnotations(true);
-			exporter.setExportTables(true);
-			exporter.setExportViews(false);
-			exporter.setTableNamePattern("MY_%");
+            // QueryDSL
+            MetaDataExporter exporter = new MetaDataExporter();
+            // exporter.setConfiguration(configuration);
+            exporter.setNamePrefix("Q");
+            // exporter.setNamingStrategy(namingStrategy);
+            exporter.setTypeMappings(new JavaTypeMappings());
+            exporter.setBeanSerializer(new ExtendedBeanSerializer());
+            exporter.setPackageName("de.freese.spring.embedded.domain");
+            exporter.setTargetFolder(new File("src/main/generated"));
+            exporter.setColumnAnnotations(true);
+            exporter.setValidationAnnotations(true);
+            exporter.setExportTables(true);
+            exporter.setExportViews(false);
+            exporter.setTableNamePattern("MY_%");
 
-			Connection connection = DataSourceUtils.getConnection(dao.getJdbcTemplate().getDataSource());
-			exporter.export(connection.getMetaData());
-		}
-	}
+            Connection connection = DataSourceUtils.getConnection(dao.getJdbcTemplate().getDataSource());
+            exporter.export(connection.getMetaData());
+        }
+    }
 }
