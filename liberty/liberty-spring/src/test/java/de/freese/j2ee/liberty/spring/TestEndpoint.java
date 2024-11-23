36761901411a3c02ad206a22771b3f0b337627a9
tfreese <community@freese-home.de>
2024-11-23 13:36:51 +0100

    spring-boot 3.4.0 migration

--- liberty/liberty-spring/src/test/java/de/freese/j2ee/liberty/spring/TestEndpoint.java
+++ liberty/liberty-spring/src/test/java/de/freese/j2ee/liberty/spring/TestEndpoint.java
@@ -16,27 +16,28 @@ import org.junit.jupiter.api.Test;
  * @author Thomas Freese
  */
 class TestEndpoint {
-    private static URI uri;
+    private static URI uriBase;
 
     @BeforeAll
     public static void init() {
         final String port = System.getProperty("http.port");
         final String context = System.getProperty("context.root");
-        uri = URI.create("http://localhost:%s/%s/sysdate".formatted(port, context));
-        System.out.printf("URL: %s%n", uri);
+        uriBase = URI.create("http://localhost:%s/%s".formatted(port, context));
     }
 
     @Test
-    void testServlet() throws Exception {
+    void testRestService() throws Exception {
+        final URI uriRestService = URI.create(uriBase.toString() + "/sysdate");
+
         try (HttpClient httpClient = HttpClient.newBuilder().build()) {
             final HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .uri(uri)
+                    .uri(uriRestService)
                     .GET()
                     .build();
 
             final HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-            assertEquals(200, httpResponse.statusCode(), "Incorrect response code from " + uri);
+            assertEquals(200, httpResponse.statusCode(), "Incorrect response code from " + uriRestService);
             assertNotNull(httpResponse.body());
         }
     }
